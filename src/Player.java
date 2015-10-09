70b2ef954f61b69eaf53751873ba067d2a074e4f
earlbozarth <ehbbozarth@gmail.com>
2015-10-09 10:48:42 -0400

    Refactor Code into Player Class

---
+++ src/Player.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * Created by earlbozarth on 10/9/15.
+ */
+public class Player {
+    String name;
+    String weapon;
+    String area;
+
+    void chooseName(){
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        name = scanner.nextLine();
+
+        System.out.println("Good luck, " + name);
+    }//End of chooseName
+
+    void chooseWeapon() throws Exception {
+        System.out.println("[1] Pick up Sword");
+        System.out.println("[2] Pick up Mallet");
+
+        Scanner scanner = new Scanner(System.in);
+
+        weapon = scanner.nextLine();
+        int weaponNum = Integer.valueOf(weapon);
+
+        if (weaponNum == 1){
+            //weapons.equals("1")
+            System.out.println("The sword is a good choice!");
+        }//End of if
+        else if(weaponNum == 2) {
+            //weapon.equals("2")
+            System.out.println("The mallet is a fine choice!");
+        }//End of else if
+        else {
+            throw new Exception("invalid weapon");
+        }
+    }//End of chooseWeapon
+
+    void chooseArea() throws Exception {
+        System.out.println("[1] Enter the Forest");
+        System.out.println("[2] Enter the underground tunnel");
+
+        Scanner scanner = new Scanner(System.in);
+
+        area = scanner.nextLine();
+        int areaNum = Integer.valueOf(area);
+
+        if (areaNum == 1){
+            System.out.println("Entering the Forest");
+        }
+        else if (areaNum == 2){
+            System.out.println("Entering the underground tunnel");
+        }
+        else {
+            throw new Exception("invalid area");
+        }
+    }//End of chooseArea
+
+}//End of Player Class
